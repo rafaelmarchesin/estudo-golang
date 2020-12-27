e600fc6da4c0d3fc59b8a981a7888d3f6657c2b4
Rafael Marchesin <rmarchesin@gmail.com>
2020-12-26 23:55:37 -0300

    feat: Exercícios de estruturas de controle

---
+++ estruturas_controle/if_else_if.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	resultado := notaParaConceito(6.5)
+	fmt.Println(resultado)
+}
+
+func notaParaConceito(n float64) string {
+	if n >= 9 && n <= 10 {
+		return "A"
+	} else if n >= 8 && n < 9 {
+		return "B"
+	} else if n >= 5 && n < 8 {
+		return "C"
+	} else if n >= 3 && n < 5 {
+		return "D"
+	} else {
+		return "E"
+	}
+}
