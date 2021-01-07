b785a16f05cad1c148b7e83f0f005b51a3e8e9a4
Rafael Marchesin <rmarchesin@gmail.com>
2021-01-06 23:38:57 -0300

    feat: Mais um pouco de Operadores

--- otavio_augusto/operadores/operadores.go
+++ otavio_augusto/operadores/operadores.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Os principais operadores de Go são bem parecidos com os da maioria das linguagens de programação
 
@@ -16,5 +18,26 @@ func main() {
 
 	//Operadores relacionais
 	//Igual, Maior que, Menor que
+	//Diferente, Maior ou igual, menor ou igual
+
+	//Operadores lógicos
+	//E (&&), ou(||), negação(!)
+
+	//Operadores unários
+	numero := 10
+	numero++
+	numero += 15
+	numero--
+	numero -= 20
+	numero *= 12
+	numero %= 3 //Módulo
+	fmt.Println(numero)
+
+	//No Go, não existe o préfixado (--numero)
+
+	//Operador ternário
+	//No Go não existe operador ternário
+	//A premissa do Go é que exista apenas uma única maneira de se fazer as coisas...
+	//Como podemos escrever o if, não existe, então o operador ternário
 
 }
