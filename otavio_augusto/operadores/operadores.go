f3cab060b694cf9b9865bc28101dadc6d4a3b2fd
Rafael Marchesin <rmarchesin@gmail.com>
2021-01-06 23:28:23 -0300

    feat: Estudo de Operadores

---
+++ otavio_augusto/operadores/operadores.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	//Os principais operadores de Go são bem parecidos com os da maioria das linguagens de programação
+
+	var num1 int16 = 10
+	var num2 int32 = 25
+
+	//Isso não pode ser feito, pois os tipos são diferentes
+	//soma := num1 + num2
+
+	_, _ = num1, num2
+
+	//Operadores de atribuição
+	//É o :=  e o "var nome tipo"
+
+	//Operadores relacionais
+	//Igual, Maior que, Menor que
+
+}
